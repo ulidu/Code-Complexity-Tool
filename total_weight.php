827008936d75b69801a1573120f39d8bc0f29938
ulidu <ulidutheerake@gmail.com>
2020-02-21 09:21:22 +0530

    Total Table

--- total_weight.php
+++ total_weight.php
@@ -62,22 +62,49 @@
 
 
     <!--begin::Portlet-->
-    <div class="kt-portlet kt-portlet--skin-solid kt-portlet-- kt-bg-brand">
+    <div style="background-color: #F4F7FF;" class="kt-portlet kt-portlet--skin-solid kt-portlet--">
         <div class="kt-portlet__head">
             <div class="kt-portlet__head-label">
-												<span class="kt-portlet__head-icon">
+												<span class="kt-portlet__head-icon kt-font-brand">
 													<i class="flaticon-time-1"></i>
 												</span>
-                <h3 class="kt-portlet__head-title">
+                <h3 class="kt-portlet__head-title kt-font-brand">
                     Total Complexity of the Program
                 </h3>
             </div>
 
         </div>
-        <div class="kt-portlet__body">
+        <div class="kt-portlet__body kt-font-brand">
 
-            <center><h2 style="font-family: 'Fira Code'">Cpr</h2></center>
-            <center><h1 style="font-family: 'Fira Code'">50</h1></center>
+
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <div class="kt-portlet kt-iconbox kt-iconbox--brand kt-iconbox--animate-slower">
+                        <div class="kt-portlet__body">
+                            <div class="kt-iconbox__body">
+                                <div class="kt-iconbox__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24" />
+                                            <path d="M2.56066017,10.6819805 L4.68198052,8.56066017 C5.26776695,7.97487373 6.21751442,7.97487373 6.80330086,8.56066017 L8.9246212,10.6819805 C9.51040764,11.267767 9.51040764,12.2175144 8.9246212,12.8033009 L6.80330086,14.9246212 C6.21751442,15.5104076 5.26776695,15.5104076 4.68198052,14.9246212 L2.56066017,12.8033009 C1.97487373,12.2175144 1.97487373,11.267767 2.56066017,10.6819805 Z M14.5606602,10.6819805 L16.6819805,8.56066017 C17.267767,7.97487373 18.2175144,7.97487373 18.8033009,8.56066017 L20.9246212,10.6819805 C21.5104076,11.267767 21.5104076,12.2175144 20.9246212,12.8033009 L18.8033009,14.9246212 C18.2175144,15.5104076 17.267767,15.5104076 16.6819805,14.9246212 L14.5606602,12.8033009 C13.9748737,12.2175144 13.9748737,11.267767 14.5606602,10.6819805 Z" fill="#000000" opacity="0.3" />
+                                            <path d="M8.56066017,16.6819805 L10.6819805,14.5606602 C11.267767,13.9748737 12.2175144,13.9748737 12.8033009,14.5606602 L14.9246212,16.6819805 C15.5104076,17.267767 15.5104076,18.2175144 14.9246212,18.8033009 L12.8033009,20.9246212 C12.2175144,21.5104076 11.267767,21.5104076 10.6819805,20.9246212 L8.56066017,18.8033009 C7.97487373,18.2175144 7.97487373,17.267767 8.56066017,16.6819805 Z M8.56066017,4.68198052 L10.6819805,2.56066017 C11.267767,1.97487373 12.2175144,1.97487373 12.8033009,2.56066017 L14.9246212,4.68198052 C15.5104076,5.26776695 15.5104076,6.21751442 14.9246212,6.80330086 L12.8033009,8.9246212 C12.2175144,9.51040764 11.267767,9.51040764 10.6819805,8.9246212 L8.56066017,6.80330086 C7.97487373,6.21751442 7.97487373,5.26776695 8.56066017,4.68198052 Z" fill="#000000" />
+                                        </g>
+                                    </svg> </div>
+                                <div class="col-lg-12">
+                                <div class="kt-iconbox__desc kt-font-brand">
+
+                                    <center><h1 style="font-family: 'Fira Code'">Cpr : 50</h1></center>
+
+
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
 
         </div>
 
@@ -180,6 +207,106 @@
                                         <td>0</td>
                                         <td style="color: white" class="kt-label-bg-color-1">2</td>
                                     </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>7</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>8</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>9</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td>10</td>
+                                        <td>class Pattern {</td>
+                                        <td>2</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td style="color: white" class="kt-label-bg-color-1">2</td>
+                                    </tr>
+
 
 
 
