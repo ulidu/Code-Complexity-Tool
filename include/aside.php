6b62a833b536b868e4f355fc502601c127ae4c9b
ulidu <ulidutheerake@gmail.com>
2020-02-27 23:23:43 +0530

    Splitting the file(s) into statements and inserting into the tables

--- include/aside.php
+++ include/aside.php
@@ -196,14 +196,11 @@
 
                             <!--begin: User Bar -->
                             <div class="kt-header__topbar-item kt-header__topbar-item--user">
-                                <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
+                                <div class="kt-header__topbar-wrapper" data-toggle="" data-offset="0px,0px">
                                     <div class="kt-header__topbar-user">
-                                        <span class="kt-hidden kt-header__topbar-welcome kt-hidden-mobile">Hi,</span>
-                                        <span class="kt-hidden kt-header__topbar-username kt-hidden-mobile">Sean</span>
-                                        <img class="kt-hidden" alt="Pic" src="assets/media/users/300_25.jpg" />
 
                                         <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
-                                        <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bolder">U</span>
+                                        <span onclick="toggleFullScreen()" class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bolder"><i class="flaticon-arrows"></i></span>
                                     </div>
                                 </div>
                                 <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
--- include/footer.php
+++ include/footer.php
@@ -88,6 +88,29 @@
         $button.prop("disabled", (this.value === "")? true : false);
     });
 </script>
+
+<script>
+function toggleFullScreen() {
+if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+(!document.mozFullScreen && !document.webkitIsFullScreen)) {
+if (document.documentElement.requestFullScreen) {
+document.documentElement.requestFullScreen();
+} else if (document.documentElement.mozRequestFullScreen) {
+document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullScreen) {
+document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+}
+} else {
+if (document.cancelFullScreen) {
+document.cancelFullScreen();
+} else if (document.mozCancelFullScreen) {
+document.mozCancelFullScreen();
+} else if (document.webkitCancelFullScreen) {
+document.webkitCancelFullScreen();
+}
+}
+}
+</script>
 	</body>
 
 	<!-- end::Body -->
