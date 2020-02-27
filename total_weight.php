f28d7bffa4a6ac7c107badcc37a676f07049b691
ulidu <ulidutheerake@gmail.com>
2020-02-27 23:43:25 +0530

    Splitting the file(s) into statements and inserting into the tables

--- total_weight.php
+++ total_weight.php
@@ -6,7 +6,7 @@
     $ds = DIRECTORY_SEPARATOR;  // Store directory separator (DIRECTORY_SEPARATOR) to a simple variable. This is just a personal preference as we hate to type long variable name.
     $storeFolder = 'uploads';   // Declare a variable for destination folder.
 
-    $tempFile = $_FILES['file']['tmp_name'];          // If file is sent to the page, store the file object to a temporary variable.
+    $tempFile = $_FILES['file']['tmp_name'];         // If file is sent to the page, store the file object to a temporary variable.
     $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  // Create the absolute path of the destination folder.
 
     $newFileName = $_FILES['file']['name'];
