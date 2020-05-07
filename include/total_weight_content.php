31fc1e10f55db27e604ba71965bc2f6744ffbbd6
fasliya <fazy.hfb@gmail.com>
2020-05-07 14:53:12 +0530

    Working on Total Weight Function

--- include/total_weight_content.php
+++ include/total_weight_content.php
@@ -29,21 +29,33 @@
     $entry_arr_af = preg_split("/\.java/", $entry);
     $entry_arr = array_filter($entry_arr_af);
 
-    foreach ($entry_arr as $files_arr) {
+    foreach ($entry_arr
+
+    as $files_arr) {
 
 
     $fi = new FilesystemIterator($storeFolder, FilesystemIterator::SKIP_DOTS);
     $limit = (iterator_count($fi));
 
     $lastRow = "SELECT * FROM ( SELECT * FROM cs ORDER BY CsID DESC LIMIT $limit) result ORDER BY CsID ASC";
-    $run_query_last = mysqli_query($con,$lastRow);
+    $run_query_last = mysqli_query($con, $lastRow);
 
     while ($lastrow = mysqli_fetch_assoc($run_query_last)) {
     $CsID_last = $lastrow['CsID'];
     $CsValue_last = $lastrow['CsValue'];
 
 
+    ?>
 
+    <?php
+    $lastRow = "SELECT * FROM ( SELECT * FROM totalcomplexity ORDER BY totalcomplexityID DESC LIMIT $limit) result ORDER BY totalcomplexityID ASC";
+    $run_query_last = mysqli_query($con, $lastRow);
+
+    while ($lastrow = mysqli_fetch_assoc($run_query_last)) {
+    $totalcomplexityID_last = $lastrow['totalcomplexityID'];
+    $totalcomplexityValue_last = $lastrow['totalcomplexityValue'];
+
+    $final_total += $totalcomplexityValue_last;
     ?>
     <!-- begin:: Content -->
     <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
@@ -54,20 +66,32 @@
                 <div class="kt-portlet__body">
                     <div class="kt-iconbox__body">
                         <div class="kt-iconbox__icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                 width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
                                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <rect x="0" y="0" width="24" height="24" />
-                                    <path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#000000" opacity="0.3" />
-                                    <path d="M10.875,15.75 C10.6354167,15.75 10.3958333,15.6541667 10.2041667,15.4625 L8.2875,13.5458333 C7.90416667,13.1625 7.90416667,12.5875 8.2875,12.2041667 C8.67083333,11.8208333 9.29375,11.8208333 9.62916667,12.2041667 L10.875,13.45 L14.0375,10.2875 C14.4208333,9.90416667 14.9958333,9.90416667 15.3791667,10.2875 C15.7625,10.6708333 15.7625,11.2458333 15.3791667,11.6291667 L11.5458333,15.4625 C11.3541667,15.6541667 11.1145833,15.75 10.875,15.75 Z" fill="#000000" />
-                                    <path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#000000" />
+                                    <rect x="0" y="0" width="24" height="24"/>
+                                    <path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z"
+                                          fill="#000000" opacity="0.3"/>
+                                    <path d="M10.875,15.75 C10.6354167,15.75 10.3958333,15.6541667 10.2041667,15.4625 L8.2875,13.5458333 C7.90416667,13.1625 7.90416667,12.5875 8.2875,12.2041667 C8.67083333,11.8208333 9.29375,11.8208333 9.62916667,12.2041667 L10.875,13.45 L14.0375,10.2875 C14.4208333,9.90416667 14.9958333,9.90416667 15.3791667,10.2875 C15.7625,10.6708333 15.7625,11.2458333 15.3791667,11.6291667 L11.5458333,15.4625 C11.3541667,15.6541667 11.1145833,15.75 10.875,15.75 Z"
+                                          fill="#000000"/>
+                                    <path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z"
+                                          fill="#000000"/>
                                 </g>
-                            </svg> </div>
+                            </svg>
+                        </div>
                         <div class="col-lg-12">
                             <div class="kt-iconbox__desc kt-font-success">
+                                <?php $lastRow = "SELECT * FROM ( SELECT * FROM totalcomplexity ORDER BY totalcomplexityID DESC LIMIT $limit) result ORDER BY totalcomplexityID ASC";
+                                $run_query_last = mysqli_query($con, $lastRow);
 
-                                <center><h1 style="font-family: 'Fira Code'">Total Complexity : <?php echo $total_CsValue; ?></h1></center>
+                                while ($lastrow = mysqli_fetch_assoc($run_query_last)) {
+                                    $totalcomplexityID_last = $lastrow['totalcomplexityID'];
+                                    $totalcomplexityValue_last = $lastrow['totalcomplexityValue']; ?>
 
+                                    <center><h1 style="font-family: 'Fira Code'">Total Complexity
+                                            : <?php echo $final_total; ?></h1></center>
 
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
@@ -118,15 +142,9 @@
                                         <div class="col-lg-12">
                                             <div class="kt-iconbox__desc kt-font-brand">
 
-                                                <?php
-                                                $lastRow = "SELECT * FROM ( SELECT * FROM totalcomplexity ORDER BY totalcomplexityID DESC LIMIT $limit) result ORDER BY totalcomplexityID ASC";
-                                                $run_query_last = mysqli_query($con,$lastRow);
 
-                                                while ($lastrow = mysqli_fetch_assoc($run_query_last)) {
-                                                $totalcomplexityID_last = $lastrow['totalcomplexityID'];
-                                                $totalcomplexityValue_last = $lastrow['totalcomplexityValue'];
-                                                ?>
-                                                <center><h1 style="font-family: 'Fira Code'">Cpr : <?php echo $totalcomplexityValue_last ?></h1></center>
+                                                <center><h1 style="font-family: 'Fira Code'">Cpr
+                                                        : <?php echo $totalcomplexityValue_last ?></h1></center>
 
 
                                             </div>
@@ -170,8 +188,6 @@
                                                     echo $file; ?>
                                                 </h3>
                                             </div>
-
-
 
 
                                             <div class="kt-portlet__head-toolbar">
@@ -281,7 +297,6 @@
                                                 $weight_operator = $Operator_last;
                                                 $weight_numerical = $NumericalValue_last;
                                                 $weight_string = $StringLiteral_last;
-
 
 
                                                 if (!$split == ''){
@@ -709,7 +724,6 @@
 
                                                 $total_TCps += $TCps;
 
-                                                $total_CsValue += $total_TCps;
 
                                                 ?>
                                                 <tr>
@@ -721,14 +735,14 @@
                                                     <td>0</td>
                                                     <td>0</td>
                                                     <td>0</td>
-                                                    <td style="color: #2A3746;font-weight: bold;" class="kt-label-bg-color-1"><?php echo $TCps; ?></td>
+                                                    <td style="color: #2A3746;font-weight: bold;"
+                                                        class="kt-label-bg-color-1"><?php echo $TCps; ?></td>
                                                     <?php $i++;
                                                     }
                                                     }
                                                     $_SESSION['row_count'] = $i;
                                                     ?>
                                                 </tr>
-
 
 
                                                 </tbody>
@@ -742,13 +756,12 @@
                                                     <th>0</th>
                                                     <th>0</th>
                                                     <th>9</th>
-                                                    <th style="font-weight: bold; font-size: x-large;" class="bg-dark kt-font-brand"><?php echo $total_TCps; ?></th>
+                                                    <th style="font-weight: bold; font-size: x-large;"
+                                                        class="bg-dark kt-font-brand"><?php echo $total_TCps; ?></th>
                                                 </tr>
                                                 </tfoot>
                                             </table>
                                             <?php
-
-                                            echo $total_CsValue;
 
                                             $query_disp_total_comp = "INSERT INTO totalcomplexity(totalcomplexityValue) VALUES('$total_TCps')";
                                             mysqli_query($con, $query_disp_total_comp);
@@ -789,7 +802,15 @@
             <!--end::Portlet-->
 
 
-            <?php }}}} ?>
+            <?php }
+            }
+            }
+            }
+
+            $query_disp_final_tot = "INSERT INTO finaltotal(FinalTotalValue) VALUES('$final_total')";
+            mysqli_query($con, $query_disp_final_tot);
+
+            ?>
 
 
         </div>
